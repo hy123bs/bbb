50044f19bc8a611525c173fffdf08e5bbfc3dfd3
hy123bs <hy123bs@gmail.com>
2019-10-05 17:06:08 +0800

    test bbb

---
+++ bbb/bbb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bbb
+//
+//  Created by summer on 2019/10/5.
+//  Copyright © 2019 summer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
